cf56f3f17ce9d84dae87ed5c145968854a200d3e
Pankaj Kumar <pankajgang070@gmail.com>
2023-02-10 01:24:26 +0530

    f-initial

---
+++ backend/src/server.js
@@ -0,0 +1,53 @@
+import express from "express";
+
+import { db, connectToDB } from "./db.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/articles/:name", async (req, res) => {
+
+    const {name} = req.params;
+    const article = await db.collection('articles').findOne({name});
+
+    if(article) {
+        res.json(article);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.put("/api/articles/:name/upvote", async (req, res)=> {
+    const {name} = req.params;
+    await db.collection('articles').updateOne({name}, { $inc: {count: 1} });
+
+    const article = await db.collection('articles').findOne({name});
+
+    if(article){
+        res.send(name + " now has " + article.count + " upvotes");
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.post("/api/articles/:name/comments", async (req,res)=> {
+        const {name} = req.params;
+        const {postedBy, text} = req.body;
+        await db.collection('articles').updateOne({name}, { $push: {comments : {postedBy, text}} });
+
+        const article = await db.collection('articles').findOne({name});
+
+        if(article){
+            res.send(article);
+        } else {
+            res.sendStatus(404);
+        }
+});
+
+
+connectToDB(() => {
+    app.listen(5000, ()=>{
+        console.log("App is listening at port 5000!");
+    })
+});
